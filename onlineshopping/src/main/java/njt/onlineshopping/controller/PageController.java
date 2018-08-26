505199cea20bf088e53a8d0f60e22171a7835f17
srdjandjuka <mrsrdjandjukanovic@gmail.com>
2018-08-26 17:10:16 +0200

    Static resources completed!

---
+++ onlineshopping/src/main/java/njt/onlineshopping/controller/PageController.java
@@ -0,0 +1,26 @@
+package njt.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = {"/", "/home", "/index"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "Hello");
+		return mv;
+	}
+	
+	@RequestMapping(value="/test")
+	public ModelAndView test(@RequestParam(value="greeting", required=false)String greeting) {
+		ModelAndView mv = new ModelAndView("page");
+		
+		mv.addObject("greeting", greeting);
+		
+		return mv;
+	}
+}
