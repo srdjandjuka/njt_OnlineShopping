1f814c2e07879126f9efbcdbeaef0f6ead036cf8
srdjandjuka <mrsrdjandjukanovic@gmail.com>
2018-08-26 15:41:27 +0200

    Project setup completed!

---
+++ shoppingbackend/src/main/java/njt/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package njt.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
